ce9899a86fd60452f011504acb5f15eaaab4171a
nithyabalajintt <nithya.balaji@nttdata.com>
2025-05-16 00:04:07 +0530

    Create error.py

---
+++ error.py
@@ -0,0 +1,26 @@
+                                                                                                         ┃
+┃ • scraping(url=https://www.investing.com/equities/asian-hotels-east-ltd-ratios, formats=markdown,        ┃
+┃ includeTags=['.indicators-table_row__Q16TJ'])                                                            ┃
+┃                                                                                                          ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+┏━ Response (18.3s) ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                                                                                                          ┃
+┃                                                                                                          ┃
+┃  {                                                                                                       ┃
+┃    "Net Profit Margin": "-13.66%",                                                                       ┃
+┃    "Return on Equity": "10.7%",                                                                          ┃
+┃    "Quick Ratio": "0.04",                                                                                ┃
+┃    "Return on Assets": "2.8%",                                                                           ┃
+┃    "Current Ratio": "0.98",                                                                              ┃
+┃    "Asset Turnover Ratio": "0.26",                                                                       ┃
+┃    "Debt Equity Ratio": "134.51%"                                                                        ┃
+┃  }                                                                                                       ┃
+┃                                                                                                          ┃
+┃                                                                                                          ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+Agent response: None
+Error or empty response from the agent.
+Data for risk analysis: {'Net Profit Margin': None, 'Return on Equity': None, 'Return on Assets': None, 'Current Ratio': None, 'Quick Ratio': None, 'Asset Turnover Ratio': None, 'Debt Equity Ratio': None, 'Loan Value': 230.0, 'Collateral Value': 340.0, 'Credit Score': 780, 'LtC': 0.6764705882352942}
+Final DataFrame:
+  Net Profit Margin Return on Equity Return on Assets  ... Collateral Value Credit Score       LtC
+0              None             None             None  ...            340.0          780  0.676471
